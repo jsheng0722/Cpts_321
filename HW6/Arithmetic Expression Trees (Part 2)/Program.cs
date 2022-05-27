327e5a5a6fc7096bd3f897b852679a2dcae7a87c
jsheng0722 <jihui.sheng@wsu.edu>
2022-05-27 20:59:27 +0800

    HW6

---
+++ HW6/Arithmetic Expression Trees (Part 2)/Program.cs
@@ -0,0 +1,71 @@
+﻿// *********************************************************
+// Class: Program
+// CptS321_HW6
+// Name: Jihui.Sheng
+// Id: 11539324
+// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+// *********************************************************
+namespace CptS321_HW6
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.InteropServices;
+    using System.Text;
+    using System.Threading.Tasks;
+    using CptS321;
+
+    /// <summary>
+    /// Program class.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main for run program.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+            ExpressionTree expressionTree = new ExpressionTree("\"A1-12-C1\"");
+            string userInput;
+            bool consoleEnd = false;
+            while (consoleEnd != true)
+            {
+                Console.WriteLine("Menu (current expression=)" + expressionTree.Expression);
+                Console.WriteLine("  1 = Enter a new expression");
+                Console.WriteLine("  2 = Set a variable value");
+                Console.WriteLine("  3 = Evaluate tree");
+                Console.WriteLine("  4 = Quit");
+                userInput = Console.ReadLine();
+                switch (userInput)
+                {
+                    case "1":
+                        Console.Write("Enter new expression: ");
+                        string inputExpression = Console.ReadLine();
+                        expressionTree = new ExpressionTree(inputExpression);
+                        continue;
+                    case "2":
+                        Console.Write("Enter variable name: ");
+                        string inputName = Console.ReadLine();
+                        Console.Write("Enter variable value: ");
+                        string inputValue = Console.ReadLine();
+                        expressionTree.SetVariable(inputName, Convert.ToDouble(inputValue));
+                        continue;
+                    case "3":
+                        Console.WriteLine(expressionTree.Evaluate());
+                        continue;
+                    case "4":
+                        Console.WriteLine("Done");
+                        consoleEnd = true;
+                        break;
+                    default:
+                        Console.WriteLine("Wrong enter, please enter the number from 1 to 4.");
+                        Console.WriteLine("-------------------------------------------------");
+                        continue;
+                }
+            }
+        }
+    }
+}
---
+++ HW6/Logic_Engine_DLL/OperatorNodeFactory.cs
@@ -0,0 +1,101 @@
+﻿// *********************************************************
+// Class: OperatorNodeFactory
+// CptS321_HW6
+// Name: Jihui.Sheng
+// Id: 11539324
+// <copyright file="OperatorNodeFactory.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+// *********************************************************
+namespace CptS321
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// OperatorNodeFactory class that has 4 operators.
+    /// </summary>
+    public class OperatorNodeFactory : Expression
+    {
+        /// <summary>
+        /// Add class.
+        /// </summary>
+        public class Add : OperatorNode
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Add"/> class.
+            /// </summary>
+            public Add()
+                : base('+')
+            {
+            }
+
+            /// <inheritdoc/>
+            public override double Evaluate()
+            {
+                return this.Left.Evaluate() + this.Right.Evaluate();
+            }
+        }
+
+        /// <summary>
+        /// Minus class.
+        /// </summary>
+        public class Minus : OperatorNode
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Minus"/> class.
+            /// </summary>
+            public Minus()
+                : base('-')
+            {
+            }
+
+            /// <inheritdoc/>
+            public override double Evaluate()
+            {
+                return this.Left.Evaluate() - this.Right.Evaluate();
+            }
+        }
+
+        /// <summary>
+        /// Multiple class.
+        /// </summary>
+        public class Multiple : OperatorNode
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Multiple"/> class.
+            /// </summary>
+            public Multiple()
+                : base('*')
+            {
+            }
+
+            /// <inheritdoc/>
+            public override double Evaluate()
+            {
+                return this.Left.Evaluate() * this.Right.Evaluate();
+            }
+        }
+
+        /// <summary>
+        /// Divide class.
+        /// </summary>
+        public class Divide : OperatorNode
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Divide"/> class.
+            /// </summary>
+            public Divide()
+                : base('/')
+            {
+            }
+
+            /// <inheritdoc/>
+            public override double Evaluate()
+            {
+                return this.Left.Evaluate() / this.Right.Evaluate();
+            }
+        }
+    }
+}
